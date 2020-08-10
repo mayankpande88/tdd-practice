30407bde5b32e9ac8836a3c2deb536c41c64172d
Maynak Pande <mayank.pande@saama.com>
2020-08-10 22:44:01 +0530

    added support to validate negative no

--- src/main/java/com/practice/tdd/Calculator.java
+++ src/main/java/com/practice/tdd/Calculator.java
@@ -1,20 +1,29 @@
 package com.practice.tdd;
 
 public class Calculator {
-    public int add(String input) {
-        String delim="\\s|,";
+    public int add(String input) throws Exception {
+        StringBuilder notValidNos = new StringBuilder();
+        String delim = "\\s|,";
         if (input.startsWith("//")) {
-            delim=input.split("\\s")[0].replace("//","");
-            input=input.substring(input.indexOf('\n')+1);
+            delim = delim+"|"+input.split("\\s")[0].replace("//", "");
+            input = input.substring(input.indexOf('\n') + 1);
         }
         String[] inputArray = input.split(delim);
-      
+
         if (inputArray.length == 1) {
             return inputArray[0] != "" ? Integer.valueOf(inputArray[0]) : 0;
         }
         int sum = 0;
         for (String s : inputArray) {
-            sum += s != "" ? Integer.valueOf(s) : 0;
+            int no = s != "" ? Integer.valueOf(s) : 0;
+            if (no < 0) {
+                notValidNos.append(String.format("%s,", no));
+                continue;
+            }
+            sum += no;
+        }
+        if(notValidNos.length()>0) {
+            throw new Exception(String.format("negatives not allowed ",notValidNos)) ;
         }
         return sum;
     }
--- src/test/java/com/practice/tdd/CalculatorTest.java
+++ src/test/java/com/practice/tdd/CalculatorTest.java
@@ -8,21 +8,33 @@ import org.junit.Test;
 public class CalculatorTest {
 
     @Test
-    public void test1() {
+    public void test1() throws Exception {
         Calculator c =new Calculator();
         assertEquals(0, c.add(""));
     }
     
     @Test
-    public void test2() {
+    public void test2() throws Exception {
         Calculator c= new Calculator();
         assertEquals(5,c.add("1,1,1,1,1"));
     }
     
     @Test
-    public void test3() {
+    public void test3() throws Exception {
         Calculator c= new Calculator();
         assertEquals(5,c.add("1\n1,1,1,1"));
         assertEquals(5,c.add("1\n1,1,1,1\n"));
+    }
+    
+    @Test
+    public void test4() throws Exception {
+        Calculator c= new Calculator();
+        assertEquals(5,c.add("//;\n1\n1;1;1;1"));
+    }
+    
+    @Test(expected =Exception.class)
+    public void test5() throws Exception {
+        Calculator c= new Calculator();
+        assertEquals(5,c.add("//;\n1\n1;1;-1;-4"));
     }
 }
